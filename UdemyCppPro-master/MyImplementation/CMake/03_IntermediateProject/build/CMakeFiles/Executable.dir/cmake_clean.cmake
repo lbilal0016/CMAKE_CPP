89ea97e5dd7e8b7fbda5fccab5fb8fce5d288db0
lbilal0016 <leventbilal.16@gmail.com>
2023-07-19 00:22:54 +0200

    build Ordner

---
+++ UdemyCppPro-master/MyImplementation/CMake/03_IntermediateProject/build/CMakeFiles/Executable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Executable.dir/app/main.cc.o"
+  "CMakeFiles/Executable.dir/app/main.cc.o.d"
+  "Executable"
+  "Executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
