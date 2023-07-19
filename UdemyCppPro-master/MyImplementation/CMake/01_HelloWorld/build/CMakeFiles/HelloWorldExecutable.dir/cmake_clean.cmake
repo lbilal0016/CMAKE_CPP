b24167bd1df91db654237ea0a92bcf72792032ab
lbilal0016 <leventbilal.16@gmail.com>
2023-07-19 12:14:31 +0200

    Missing build directories are added

---
+++ UdemyCppPro-master/MyImplementation/CMake/01_HelloWorld/build/CMakeFiles/HelloWorldExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldExecutable.dir/main.cc.o"
+  "CMakeFiles/HelloWorldExecutable.dir/main.cc.o.d"
+  "HelloWorldExecutable"
+  "HelloWorldExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorldExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
