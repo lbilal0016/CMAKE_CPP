8c4daa2b5af4b1685249bef25e3dfc7476d7426d
lbilal0016 <leventbilal.16@gmail.com>
2023-07-22 23:00:54 +0200

    Chapter 06 was added.

---
+++ UdemyCppPro-master/MyImplementation/CMake/06_BetterProjectStructure/app/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "my_lib.h"
+#include <config.hpp>
+
+int main()
+{
+    std::cout <<  "Project Name: " << project_name << std::endl;
+    print_hello_world();
+
+    return 0;
+}
---
+++ UdemyCppPro-master/MyImplementation/CMake/06_BetterProjectStructure/src/my_lib/my_lib.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "my_lib.h"
+
+void print_hello_world()
+{
+    std::cout << "Execution of project 04_VariabblesOptions is successful.\n";
+}
