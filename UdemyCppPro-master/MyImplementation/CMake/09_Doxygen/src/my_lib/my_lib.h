d048ccc33bdce805fdf141485b56ba80da24e69e
lbilal0016 <leventbilal.16@gmail.com>
2023-07-24 11:08:53 +0200

    Doxygen

--- UdemyCppPro-master/MyImplementation/CMake/09_Doxygen/src/my_lib/my_lib.h
+++ UdemyCppPro-master/MyImplementation/CMake/09_Doxygen/src/my_lib/my_lib.h
@@ -1,5 +1,17 @@
 #pragma once
+//  /** ... */ -> Template for doxygen documentation generation
+//  @brief -> Gives a short summary of what the function does in doxygen documentation
+
+/**
+ * @brief Prints out hello world to the console
+ *
+ */
 
 void print_hello_world();
+
+/**
+ * @brief Prints out the json version to the console
+ *
+ */
 void print_json_version();
 
